3323422855d58ac878cd1df7cb3f32a30ad89234
Slime Goblin <jshermanwilliams@gmail.com>
2024-09-21 10:16:09 -0700

    watched and remove button added, removed button removes from library too

--- java.js
+++ java.js
@@ -1,9 +1,11 @@
 /* To Do List:
+Try and make a loop that constructs objects in addCard()
+
 -Don't allow submit when empty inputs
 
 -Make new Book button that opens our form
 -Add Toggle to remove book from Library
--Add Button to change display to read
+-Add Button to change display to read(try and build this into prototype)
 
 -Make a seperate function for Adding New Movies
 -Style CSS Better(FontFamily, Card Container Grid, Title, Form, Color Scheme))
@@ -27,7 +29,7 @@ const container = document.getElementById("movieContainer");
 
 
 
-function Movie(title, genre, director, runtime) {
+function Movie(title, genre, director, runtime, watched) {
     this.title = title;
     this.genre = genre;
     this.director = director;
@@ -52,11 +54,13 @@ form.addEventListener('click', function(e){
         genre = document.getElementById("genre").value,
         director= document.getElementById("director").value,
         runTime = document.getElementById("runTime").value,
-        watched = true
+        watched = true,
     )
 
         myLibrary.push(movie);
         console.log(myLibrary);
+
+        
 
         const content = document.createElement("div");
         content.classList.add("card");
@@ -83,6 +87,35 @@ form.addEventListener('click', function(e){
         cardLabelWatched.setAttribute("id", "watchedButton")
         cardLabelWatched.textContent="Watched"
 
+        cardLabelWatched.addEventListener('click', function(){
+            console.log('hi')
+            console.log(watched)
+            if(watched == true){
+                watched = false
+                cardLabelWatched.textContent = "Unwatched"
+                content.classList.add("unwatched")
+            }
+            else if(watched ==false){
+                watched = true
+                console.log('bye')
+                cardLabelWatched.textContent="Watched"
+                content.classList.remove("unwatched")
+
+            }
+        })
+
+        const cardLabelRemove = document.createElement("button")
+        cardLabelRemove.classList.add("cardLabel");
+        cardLabelRemove.setAttribute("id", "removeButton")
+        cardLabelRemove.textContent="Remove"
+
+        
+        cardLabelRemove.addEventListener('click', function(){
+            content.remove();
+            myLibrary.splice([i], 1)
+           console.log(myLibrary);
+        })
+
 
 
         content.appendChild(cardLabelTitle)
@@ -90,11 +123,13 @@ form.addEventListener('click', function(e){
         content.appendChild(cardLabelDirector)
         content.appendChild(cardLabelRunTime)
         content.appendChild(cardLabelWatched)
+        content.appendChild(cardLabelRemove)
 
         container.appendChild(content);
 
 
 clearInputs();
+
 })
 
 function addCard(){
@@ -105,11 +140,14 @@ function addCard(){
         const content = document.createElement("div");
         content.classList.add("card");
 
-        var title = myLibrary[i].title;
-        var genre = myLibrary[i].genre;
-        var director = myLibrary[i].director;
-        var runTime = myLibrary[i].runTime;
-        var watched = myLibrary[i].watched;
+        const movie = new Movie(
+
+        title = myLibrary[i].title,
+        genre = myLibrary[i].genre,
+        director = myLibrary[i].director,
+        runTime = myLibrary[i].runTime,
+        watched = myLibrary[i].watched,
+        )
 
         const cardLabelTitle = document.createElement("div");
         cardLabelTitle.classList.add("cardLabel");
@@ -138,14 +176,30 @@ function addCard(){
             if(watched == true){
                 watched = false
                 cardLabelWatched.textContent = "Unwatched"
+                content.classList.add("unwatched")
             }
             else if(watched ==false){
                 watched = true
                 console.log('bye')
                 cardLabelWatched.textContent="Watched"
-
+                content.classList.remove("unwatched")
+                myLibrary.splice[[i],1]
+                console.log(myLibrary)
             }
         })
+
+        const cardLabelRemove = document.createElement("button")
+        cardLabelRemove.classList.add("cardLabel");
+        cardLabelRemove.setAttribute("id", "removeButton")
+        cardLabelRemove.textContent="Remove"
+
+        
+        cardLabelRemove.addEventListener('click', function(){
+            content.remove();
+            myLibrary.splice([i], 1)
+           console.log(myLibrary);
+        })
+
 
       
 
@@ -154,6 +208,7 @@ function addCard(){
         content.appendChild(cardLabelDirector)
         content.appendChild(cardLabelRunTime)
         content.appendChild(cardLabelWatched)
+        content.appendChild(cardLabelRemove)
 
         container.appendChild(content);
 
